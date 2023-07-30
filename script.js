4892d45a1b3e72e54a021b14d06c552d5602b3cf
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2023-07-30 23:34:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,22 @@ window.onload = function() {
     fetch('https://vidsrc.me/movies/latest/page-1.json')
       .then(response => response.json())
       .then(data => {
-        // Display the list of movies
-        data.movies.forEach(movie => {
-          const movieButton = document.createElement('button');
-          movieButton.textContent = movie.title;
-          movieButton.addEventListener('click', () => {
-            streamMovie(movie.imdb);
+        console.log(data); // Log the API response to the console
+  
+        // Check if data.movies exists before trying to use it
+        if (data.movies) {
+          // Display the list of movies
+          data.movies.forEach(movie => {
+            const movieButton = document.createElement('button');
+            movieButton.textContent = movie.title;
+            movieButton.addEventListener('click', () => {
+              streamMovie(movie.imdb);
+            });
+            movieListDiv.appendChild(movieButton);
           });
-          movieListDiv.appendChild(movieButton);
-        });
+        } else {
+          console.error('Error: data.movies is undefined');
+        }
       })
       .catch(error => console.error('Error:', error));
   
