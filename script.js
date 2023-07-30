f4b02dc247b591cbfac587df9437004851280888
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2023-07-30 23:31:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 window.onload = function() {
-    // Replace with your own list of movies
-    const movies = ['Movie1', 'Movie2', 'Movie3'];
-  
     const movieListDiv = document.getElementById('movieList');
     const moviePlayerDiv = document.getElementById('moviePlayer');
   
-    // Display the list of movies
-    movies.forEach(movie => {
-      const movieButton = document.createElement('button');
-      movieButton.textContent = movie;
-      movieButton.addEventListener('click', () => {
-        streamMovie(movie);
-      });
-      movieListDiv.appendChild(movieButton);
-    });
+    // Fetch the list of latest movies
+    fetch('https://vidsrc.me/movies/latest/page-1.json')
+      .then(response => response.json())
+      .then(data => {
+        // Display the list of movies
+        data.movies.forEach(movie => {
+          const movieButton = document.createElement('button');
+          movieButton.textContent = movie.title;
+          movieButton.addEventListener('click', () => {
+            streamMovie(movie.imdb);
+          });
+          movieListDiv.appendChild(movieButton);
+        });
+      })
+      .catch(error => console.error('Error:', error));
   
-    function streamMovie(movie) {
+    function streamMovie(imdb) {
       // Use the VidSrc API to get the streaming URL
-      // This is a simplified example and you'll need to replace with the actual API call
-      const streamingUrl = `https://api.vidsrc.me/link/${movie}`;
+      const streamingUrl = `https://vidsrc.me/embed/movie?imdb=${imdb}`;
   
       moviePlayerDiv.innerHTML = '';
       const iframe = document.createElement('iframe');
