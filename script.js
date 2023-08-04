b4b2d09fb5c62a56db8b31dc81f26137d4fd7285
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2023-08-04 23:14:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
+// JavaScript
 window.onload = function() {
-    const movieListDiv = document.getElementById('movieList');
-    const moviePlayerDiv = document.getElementById('moviePlayer');
-  
-    // Fetch the list of latest movies
-    fetch('https://vidsrc.me/movies/latest/page-1.json')
-      .then(response => response.json())
-      .then(data => {
-        // Check if data exists before trying to use it
-        if (data) {
-          // Display the list of movies
-          data.forEach(movie => {
-            const movieButton = document.createElement('button');
-            movieButton.textContent = movie.title;
-            movieButton.addEventListener('click', () => {
-              streamMovie(movie.imdb);
-            });
-            movieListDiv.appendChild(movieButton);
-          });
-        } else {
-          console.error('Error: data is undefined');
-        }
-      })
-      .catch(error => console.error('Error:', error));
-  
-    function streamMovie(imdb) {
-      // Use the VidSrc API to get the streaming URL
-      const streamingUrl = `https://vidsrc.me/embed/movie?imdb=${imdb}`;
-  
-      moviePlayerDiv.innerHTML = '';
-      const iframe = document.createElement('iframe');
-      iframe.src = streamingUrl;
-      moviePlayerDiv.appendChild(iframe);
-    }
+  const movieListDiv = document.getElementById('movieList');
+  const moviePlayerDiv = document.getElementById('moviePlayer');
+
+  // Fetch the list of latest movies
+  fetchLatestMovies()
+    .then(movies => {
+      if (movies && movies.length > 0) {
+        displayMovieList(movies);
+      } else {
+        console.error('Error: No movies found');
+      }
+    })
+    .catch(error => console.error('Error:', error));
+
+  async function fetchLatestMovies() {
+    const response = await fetch('https://vidsrc.me/movies/latest/page-1.json');
+    const data = await response.json();
+    return data || [];
   }
-  
+
+  function displayMovieList(movies) {
+    movies.forEach(movie => {
+      const movieButton = createMovieButton(movie.title);
+      movieButton.addEventListener('click', () => {
+        streamMovie(movie.imdb);
+      });
+      movieListDiv.appendChild(movieButton);
+    });
+  }
+
+  function createMovieButton(title) {
+    const movieButton = document.createElement('button');
+    movieButton.textContent = title;
+    return movieButton;
+  }
+
+  function streamMovie(imdb) {
+    const streamingUrl = `https://vidsrc.me/embed/movie?imdb=${imdb}`;
+
+    moviePlayerDiv.innerHTML = '';
+    const iframe = document.createElement('iframe');
+    iframe.src = streamingUrl;
+    moviePlayerDiv.appendChild(iframe);
+  }
+};
